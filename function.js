7a4ee0c8bfda703fd0c78886c43d75bd4399c6d9
ruhitbaidya <ruhitbaidya01@gmail.com>
2024-02-03 19:38:42 +0600

    this process to worded

---
+++ function.js
@@ -0,0 +1,24 @@
+const price = 45;
+
+
+// function declaer 
+function doSomthing(){
+    console.log("here call function and show output")
+}
+
+// function call
+doSomthing();
+
+
+
+// function with perams and return value
+
+function findNumber(num){
+    // there was type any js code and then run all of the code
+    return num + num
+}
+
+// call funtion with argument
+const result = findNumber(20);
+
+console.log(result)
